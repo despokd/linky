d40d2c96fa4e291c32cdc255f7f168d828a03ee1
Kilian Domaratius <github@kdomaratius.de>
2023-10-31 19:19:51 +0100

    fix typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/header.svelte';
+	import Footer from '$lib/components/footer.svelte';
 </script>
 
 <svelte:head>
