bce22584c4771435d55d014bfadc8b9c50ecc5a1
Kilian Domaratius <github@kdomaratius.de>
2023-11-06 22:54:28 +0100

    add dynamic icons

--- src/lib/components/link.svelte
+++ src/lib/components/link.svelte
@@ -1,20 +1,34 @@
 <script>
 	import { GradientButton } from 'flowbite-svelte';
-	import { GithubSolid, LinkedinSolid } from 'flowbite-svelte-icons';
+	import '@fortawesome/fontawesome-free/css/all.min.css';
 
 	export let /** @type string */ name, /** @type string */ url;
+
+	let iconClass = 'fa-brands fa-' + name.toLowerCase();
+	switch (name.toLowerCase()) {
+		case 'tequilian.de':
+			iconClass = 'fa-regular fa-lemon';
+			break;
+		case 'kdomaratius.de':
+			iconClass = 'fa-regular fa-user';
+			break;
+	}
 </script>
 
 <GradientButton outline shadow size="xl" href={url} rel="noopener noreferrer" class="block my-3">
 	<div class="flex justify-between w-full gap-1">
 		<div class="w-5 h-5">
-			{#if name.toLowerCase() === 'github'}
-				<GithubSolid />
-			{:else if name.toLowerCase() === 'linkedin'}
-				<LinkedinSolid />
-			{/if}
+			<slot name="icon">
+				<i class="{iconClass}"></i>
+			</slot>
 		</div>
 		<div>{name}</div>
 		<div class="w-5 h-5" />
 	</div>
 </GradientButton>
+
+<style>
+	i {
+		font-size: 1.25rem;
+	}
+</style>
