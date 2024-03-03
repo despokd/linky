99bb7c75780c9848f889d69b783f90a055fa6ce4
despokd <github@kdomaratius.de>
2024-03-03 09:58:33 +0100

    add gravatar size + fallback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,15 +18,15 @@
 	<meta property="og:description" content="{$_('common.description')}" />
 	<meta property="og:url" content="https://linky.kdomaratius.de" />
 	{#if data.me.image}
-		<meta property="twitter:image" content="{data.me.image}" />
+		<meta property="twitter:image" content="{data.me.image}?s=800&d=identicon" />
 		<meta property="twitter:card" content="summary_large_image">
-		<meta property="og:image" content="{data.me.image}" />
+		<meta property="og:image" content="{data.me.image}?s=800&d=identicon" />
 	{/if}
 </svelte:head>
 
 <section class="mx-auto w-full max-w-sm">
 	{#if data.me.image}
-		<img src={data.me.image + '?s=200'} alt={data.me.name} class="mx-auto rounded-full w-36 h-36 m-4" />
+		<img src={data.me.image + '?s=200&d=identicon'} alt={data.me.name} class="mx-auto rounded-full w-36 h-36 m-4" />
 	{/if}
 	<h1 class="text-center text-2xl font-semibold mx-1">{data.me.name}</h1>
 	<p class="text-center p-4 mb-4">{getTranslation(data.me.translations).description_short}</p>
